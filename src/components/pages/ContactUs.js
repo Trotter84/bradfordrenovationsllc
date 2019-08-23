ba876040438dfb8d1fea178e59323c7b8d92ea31
Daniel Trotter <flare84@gmail.com>
2019-08-23 14:55:09 -0600

    contact field test

--- src/components/pages/ContactUs.js
+++ src/components/pages/ContactUs.js
@@ -9,18 +9,12 @@ class ContactUs extends Component {
     return(
       <div id='contactUsContainer'>
         <div id='contactUsSubContainer'>
-          <form name="contact" method="POST" data-netlify="true">
+          <form name="contact" action="POST" data-netlify="true">
             <p>
               <label>Your Name: <input type="text" name="name" /></label>
             </p>
             <p>
               <label>Your Email: <input type="email" name="email" /></label>
-            </p>
-            <p>
-              <label>Your Role: <select name="role[]" multiple>
-                <option value="leader">Leader</option>
-                <option value="follower">Follower</option>
-              </select></label>
             </p>
             <p>
               <label>Message: <textarea name="message"></textarea></label>
