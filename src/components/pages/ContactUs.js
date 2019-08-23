74d2946bdbecce83b0414b671a53b43411a8f69a
Daniel Trotter <flare84@gmail.com>
2019-08-23 14:16:24 -0600

    contact field changed

--- src/components/pages/ContactUs.js
+++ src/components/pages/ContactUs.js
@@ -9,12 +9,21 @@ class ContactUs extends Component {
     return(
       <div id='contactUsContainer'>
         <div id='contactUsSubContainer'>
-          <form name="contact" netlify>
+          <form name="contact" method="POST" data-netlify="true">
             <p>
-              <label>Name <input type="text" name="name" /></label>
+              <label>Your Name: <input type="text" name="name" /></label>
             </p>
             <p>
-              <label>Email <input type="email" name="email" /></label>
+              <label>Your Email: <input type="email" name="email" /></label>
+            </p>
+            <p>
+              <label>Your Role: <select name="role[]" multiple>
+                <option value="leader">Leader</option>
+                <option value="follower">Follower</option>
+              </select></label>
+            </p>
+            <p>
+              <label>Message: <textarea name="message"></textarea></label>
             </p>
             <p>
               <button type="submit">Send</button>
