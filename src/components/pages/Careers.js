ad684f8e2d40e4f9202a65801431120f09924880
Daniel Trotter <flare84@gmail.com>
2019-08-31 20:22:14 -0600

    career form touched up. Still having some issues but going to clean up console errors first

--- src/components/pages/Careers.js
+++ src/components/pages/Careers.js
@@ -17,13 +17,13 @@ class Careers extends Component {
             <div id='careerFormLeftSide'>
                 <label id="careerName">Name: <input type="text" name="name" required /></label>
                 <label id="careerEmail">Email: <input type="email" name="email" required /></label>
-                <label id="careerPhone">Phone: <input type="tel" name="phone" placeholder="012-345-6789" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required /></label>
+                <label id="careerPhone">Phone: <input type="tel" name="phone" placeholder="012-345-6789" pattern="[0-9]{3}[0-9]{3}[0-9]{4}" required /></label>
             </div>
             <div id='careerFormRightSide'>
-                <label id="careerResume">Resume: <input type="file" name="resume" /></label>
-                <label id="careerMessage">Message: <textarea name="message"></textarea></label>
-            </div>
+              <label id="careerResume">Resume: <input type="file" name="resume" /></label>
+              <label id="careerMessage">Message: <textarea name="message"></textarea></label>
               <button type="submit" id="careerSubmit">Send</button>
+            </div>
           </form>
         </div>
       </div>
