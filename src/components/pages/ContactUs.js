b7c78f8b564628168b79a91911cbf8b8ae273f0b
Daniel Trotter <flare84@gmail.com>
2019-09-25 13:31:51 -0600

    will come back to this contact field

--- src/components/pages/ContactUs.js
+++ src/components/pages/ContactUs.js
@@ -14,6 +14,7 @@ class ContactUs extends Component {
         <form name="bradfordContact" netlify netlify-honeypot="bot-field" hidden>
           <input type="text" name="name" />
           <input type="email" name="email" />
+          <input type="tel" name="phone" />
           <textarea name="message"></textarea>
         </form>
 
@@ -22,17 +23,33 @@ class ContactUs extends Component {
         </div>
 
         <div id='contactUsSubContainer'>
-        
-          <div>
-            <form id='contactForm' name="bradfordContact" method="POST" data-netlify="true">
-            <input type="hidden" name="form-name" value="contact" />
-                <label id="contactName">Name: <input type="text" name="name" /></label>
-                <label id="contactEmail">Email: <input type="email" name="email" /></label>
-                <label id="contactPhone">Phone: <input type="tel" name="phone" placeholder="012-345-6789" pattern="[0-9]{3}[0-9]{3}[0-9]{4}" required /></label>
-                <label id="contactMessage">Message: <textarea name="message"></textarea></label>
-                <button type="submit" id="contactSubmit">Send</button>
-            </form>
-          </div>
+
+          <NetlifyForm name="bradfordContact" method="POST" data-netlify="true">
+            {({ loading, error, success }) => (
+              <div>
+                {loading &&
+                  <div>Loading...</div>
+                }
+                {error &&
+                  <div>Your information was not sent. Please try again later.</div>
+                }
+                {success &&
+                  <div>Thank you for contacting us!</div>
+                }
+                {!loading && !success &&
+                  <div id='contactForm'>
+                      <input type="hidden" name="form-name" value="contact" />
+                      <label id="contactName">Name: <input type="text" name="name" /></label>
+                      <label id="contactEmail">Email: <input type="email" name="email" /></label>
+                      <label id="contactPhone">Phone: <input type="tel" name="phone" placeholder="012-345-6789" pattern="[0-9]{3}[0-9]{3}[0-9]{4}" required /></label>
+                      <label id="contactMessage">Message: <textarea name="message"></textarea></label>
+                      <button type="submit" id="contactSubmit">Send</button>
+                  </div>
+                }
+              </div>
+            )}
+          </NetlifyForm>
+
 
           <div id="contactPhotoContainer">
             <Image id="contactPhoto" src={SidePhoto} />
