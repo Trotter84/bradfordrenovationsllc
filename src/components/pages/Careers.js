f0a87f363e55341a79eeed0ac1477477758239c0
Daniel Trotter <flare84@gmail.com>
2019-09-30 18:09:02 -0600

    career form attempt

--- src/components/pages/Careers.js
+++ src/components/pages/Careers.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import NetlifyForm from 'react-netlify-form'
 
 
 class Careers extends Component {
@@ -10,21 +11,38 @@ class Careers extends Component {
         <div id='careerTitleContainer'>
           <h2 id='careerTitle'>Join our team!</h2>
         </div>
-
         <div id='careerSubContainer'>
-          <form id='careerForm' name="bradfordContact" method="POST" data-netlify="true">
-          <input type="hidden" name="form-name" value="contact" />
-            <div id='careerFormLeftSide'>
-                <label id="careerName">Name: <input type="text" name="name" required /></label>
-                <label id="careerEmail">Email: <input type="email" name="email" required /></label>
-                <label id="careerPhone">Phone: <input type="tel" name="phone" placeholder="012-345-6789" pattern="[0-9]{3}[0-9]{3}[0-9]{4}" required /></label>
-            </div>
-            <div id='careerFormRightSide'>
-              <label id="careerResume">Resume: <input type="file" name="resume" /></label>
-              <label id="careerMessage">Message: <textarea name="message"></textarea></label>
-              <button type="submit" id="careerSubmit">Send</button>
-            </div>
-          </form>
+
+          <NetlifyForm name="bradfordCareer" method="POST" data-netlify="true">
+            {({ loading, error, success }) => (
+              <div>
+                {loading &&
+                  <div>Loading...</div>
+                }
+                {error &&
+                  <div>Your information was not sent. Please try again later.</div>
+                }
+                {success &&
+                  <div>Thank you for contacting us!</div>
+                }
+                {!loading && !success &&
+                  <div id='careerForm'>
+                  <div id='careerFormLeftSide'>
+                      <label id="careerName">Name: <input type="text" name="name" required /></label>
+                      <label id="careerEmail">Email: <input type="email" name="email" required /></label>
+                      <label id="careerPhone">Phone: <input type="tel" name="phone" placeholder="012-345-6789" pattern="[0-9]{3}[0-9]{3}[0-9]{4}" required /></label>
+                  </div>
+                  <div id='careerFormRightSide'>
+                    <label id="careerResume">Resume: <input type="file" name="resume" /></label>
+                    <label id="careerMessage">Message: <textarea name="message"></textarea></label>
+                    <button type="submit" id="careerSubmit">Send</button>
+                  </div>
+
+                  </div>
+                }
+              </div>
+            )}
+          </NetlifyForm>
         </div>
       </div>
     )
