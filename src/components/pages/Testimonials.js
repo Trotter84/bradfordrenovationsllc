2e26fb1c56ab18ef8757b6f486085e0cd93715e5
Daniel Trotter <flare84@gmail.com>
2019-12-05 11:51:37 -0700

    created testimonial page

---
+++ src/components/pages/Testimonials.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class Testimonials extends Component {
+
+  render() {
+    return (
+      <>
+        <div id='aboutUsContainer'>
+
+          <div id='aboutUsWhiteContainer'>
+
+          </div>
+        </div>
+      </>
+    )
+  }
+}
+
+
+export default Testimonials;
