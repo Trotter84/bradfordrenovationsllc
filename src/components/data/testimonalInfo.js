a2046e6cb60e3a4e15c394b34a42fc2aa3c752c5
Daniel Trotter <flare84@gmail.com>
2019-12-06 13:23:54 -0700

    testimonials added

---
+++ src/components/data/testimonalInfo.js
@@ -0,0 +1,17 @@
+const testimonialInfo = [
+  {
+    name: 'Bill',
+    quote: [
+      'Hi Drew -',
+      'The check is in the mail!',
+      'Sari and I LOVE the bathroom! It is everything we hoped it would be. As I said to you on the phone, we are so pleased by the quality of work and the attention to detail you and your guys put into our space. There is a standard that you maintain and your guys know that and follow it. Little details were given great attention and you endeavored to get all aspects of things as good as possible (especially given the challenges of an old home). Also, you were so great listening and working with us on the vision of the space (even for a small space) and working with us on the various adjustments and changes that came along the way (even if they did give you occasional heart palpitations!). You were also soooooo communicative. I have heard horror stories of people trying to get in touch with their contractors and that was certainly not the case here. I kept telling people that our contractor was calling me more than I was calling him :).',
+      'While it was nice to get up a little later this morning...I will say we kinda missed having Justin and Sean walk through the door at 7 am sharp! They were GREAT guys! Both very friendly and professional. Justin rolled with my slightly "OCD" double-checking of things, he answered all my questions, made slight adjustments to things when asked, and simply did great work. And, his coming back on Friday evening to adjust the floor tile was a testament to his and your getting things right! Sean was also a wonderful, hard working spirit to have around and our dog Albus misses him!',
+      'Finally, and certainly not least importantly, you worked with us on our budget and you were very transparent about things along the way (I know you will miss my "so what do you think that will run us" questions!). And, even accounting for the various changes that emerged, you came in very close to your original estimate - which is kind of remarkable. Our sincere thanks to you and your crew!',
+      'Would love to get you and your wife to the opera. I will send you another email with some thoughts and options.',
+      'If anything comes up I will let you know. The quarter round shelves arrived yesterday (literally 2 hours after you left). We are going too wait a few days to see if we really want/need them. If we need them zipped in, I will give you a buzz.',
+      'Thanks again Drew!'
+    ]
+  }
+]
+
+export default testimonialInfo;
--- src/components/pages/Testimonials.js
+++ src/components/pages/Testimonials.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 
+import testimonialInfo from '../data/testimonalInfo';
+
 
 class Testimonials extends Component {
 
   render() {
     return (
       <>
-        <div id='aboutUsContainer'>
-
-          <div id='aboutUsWhiteContainer'>
-
+        <div id='testimonialContainer'>
+          <div id='testimonialWhiteContainer'>
+            {testimonialInfo.map(testimonial => {
+              return(
+                <div id='quoteContainer'>
+                  <p id='quoteText'>{testimonial.quote}</p>
+                  <h4 id='quoteName'>-{testimonial.name}</h4>
+                </div>
+              )
+            })}
           </div>
         </div>
       </>
