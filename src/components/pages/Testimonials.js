1ee366b43548d617bcf2a971c41b19b0e4232284
Daniel Trotter <flare84@gmail.com>
2019-12-11 14:50:19 -0700

    testimonial setup

--- src/components/pages/Testimonials.js
+++ src/components/pages/Testimonials.js
@@ -13,7 +13,14 @@ class Testimonials extends Component {
             {testimonialInfo.map(testimonial => {
               return(
                 <div id='quoteContainer'>
-                  <p id='quoteText'>{testimonial.quote}</p>
+                  <p id='quoteText'>{testimonial.quote1}</p>
+                  <p id='quoteText'>{testimonial.quote2}</p>
+                  <p id='quoteText'>{testimonial.quote3}</p>
+                  <p id='quoteText'>{testimonial.quote4}</p>
+                  <p id='quoteText'>{testimonial.quote5}</p>
+                  <p id='quoteText'>{testimonial.quote6}</p>
+                  <p id='quoteText'>{testimonial.quote7}</p>
+                  <p id='quoteText'>{testimonial.quote8}</p>
                   <h4 id='quoteName'>-{testimonial.name}</h4>
                 </div>
               )
