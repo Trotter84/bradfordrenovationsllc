aaab9c8003981bcd901d67dc66c1841f946d9e43
Daniel Trotter <flare84@gmail.com>
2020-04-06 12:52:08 -0600

    shortened testimonial

--- src/components/pages/Testimonials.js
+++ src/components/pages/Testimonials.js
@@ -13,14 +13,7 @@ class Testimonials extends Component {
             {testimonialInfo.map(testimonial => {
               return(
                 <div id='quoteContainer'>
-                  <p id='quoteText'>{testimonial.quote1}</p>
-                  <p id='quoteText'>{testimonial.quote2}</p>
-                  <p id='quoteText'>{testimonial.quote3}</p>
-                  <p id='quoteText'>{testimonial.quote4}</p>
-                  <p id='quoteText'>{testimonial.quote5}</p>
-                  <p id='quoteText'>{testimonial.quote6}</p>
-                  <p id='quoteText'>{testimonial.quote7}</p>
-                  <p id='quoteText'>{testimonial.quote8}</p>
+                  <p id='quoteText'>{testimonial.quote}</p>
                   <h4 id='quoteName'>-{testimonial.name}</h4>
                 </div>
               )
