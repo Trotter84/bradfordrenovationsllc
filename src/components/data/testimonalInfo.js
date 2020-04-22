e2daa82cd0b3305e49bbb3b35803c0f593c28182
Daniel Trotter <flare84@gmail.com>
2020-04-21 18:06:10 -0600

    added a new testimonial

--- src/components/data/testimonalInfo.js
+++ src/components/data/testimonalInfo.js
@@ -1,7 +1,11 @@
 const testimonialInfo = [
   {
     name: 'Bill',
-    quote: 'Sari and I LOVE the bathroom! It is everything we hoped it would be...we are so pleased by the quality of work and the attention to detail you and your guys put into our space. There is a standard that you maintain and your guys know that and follow it. Also, you were so great listening and working with us...on our budget and you were very transparent about things along the way.... Our sincere thanks to you and your crew!',
+    quote: '\"Sari and I LOVE the bathroom! It is everything we hoped it would be...we are so pleased by the quality of work and the attention to detail you and your guys put into our space. There is a standard that you maintain and your guys know that and follow it. Also, you were so great listening and working with us...on our budget and you were very transparent about things along the way.... Our sincere thanks to you and your crew!\"',
+  },
+  {
+    name: 'Charlie',
+    quote: '\"Bradford Renovations has done an extensive amount of work for us. Two full beautiful bathrooms, Kitchen, Flooring, well crafted molding and trim, superb painting and thorough cleanup. Their crew was always personable but professional. Willing to adjust to a somewhat picky homeowner, our projects all turned out better than we could have hoped. We would recommend Bradford Renovations on any level of need you may have. We felt involved, respected for our opinion, and happy with the results of a job fairly priced and well done.  Every. Time.\"'
   }
 ]
 
