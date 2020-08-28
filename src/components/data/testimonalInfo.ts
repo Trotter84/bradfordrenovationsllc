bc5797cef0d4dc8e1e0f08743362c0423a4ef79a
Daniel Trotter <flare84@gmail.com>
2020-08-28 13:36:53 -0600

    added new testimonial

--- src/components/data/testimonalInfo.ts
+++ src/components/data/testimonalInfo.ts
@@ -6,6 +6,10 @@ const testimonialInfo = [
   {
     name: 'Charlie',
     quote: '"Bradford Renovations has done an extensive amount of work for us. Two full beautiful bathrooms, kitchen, flooring, well-crafted molding and trim, superb painting and thorough cleanup. Their crew was always personable but professional, willing to adjust to a somewhat picky homeowner! Our projects all turned out better than we could have hoped. We felt involved, respected for our opinions, and happy with the results of a job fairly priced and well done. We would recommend Bradford Renovations for whatever need you may have."'
+  },
+  {
+    name: 'Melanie',
+    quote: '"Drew Bradford is the ultimate professional! After having a contractor walk out on an unfinished bathroom & laundry room, we found Drew and have been so pleased with the home projects he has done for us. Owning an old Pittsburgh home there are always things that need to be done and we are so thankful to have Drew on our team! Drew shows up when he says he will, his crew is always kind and hard-working and when they start a job I always know it is going to be done well, right and complete! If I had to pick my favorite part about working with Drew it is the communication. He explains everything and communicates with us through the whole process & he is never condescending! Some contractors I have worked with like to tell you how good they are - Drew’s work SHOWS you how good he is!"'
   }
 ]
 
