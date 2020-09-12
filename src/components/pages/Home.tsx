94fae4f10fbd2fbaab2f646a3a5b3e1864154ee7
Daniel Trotter <flare84@gmail.com>
2020-09-12 13:56:05 -0600

    working on quote button

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Link } from 'react-router-dom';
+import Styled from 'styled-components';
 
 import JeffsDen2 from '../../images/IMG_8042.CR2-ed.jpg';
 import Kitchen from '../../images/kitchenBackgroundFULL.jpg';
@@ -11,11 +13,35 @@ import FlowerGarden from '../../images/flowerGarden.jpg';
 import LaundryRoom from '../../images/IMG_8322.jpg';
 
 
+const QuoteButton = Styled.button`
+  margin-top: 2em;
+  margin-left: 14em;
+  width: 200px;
+  height: 50px;
+  font-size: 20px;
+  appearance: none;
+  border: none;
+  border-radius: 12px;
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
+  cursor: pointer;
+  :hover {
+    opacity: 0.7;
+  }
+
+`
+
+
 class Home extends Component {
 
   render() {
     return(
       <div id='homeMainContainer'>
+
+        <Link to={'/contact-us'}>
+          <QuoteButton>Get A Quote Now</QuoteButton>
+        </Link>
+
+
         <div id='carouselContainer'>
           <div id='carouselSubContainer'>
             <Carousel autoPlay infiniteLoop interval={4000} showThumbs={false} showIndicators={false}>
